abad0569c15be0bf4eb03469636edcaa05fbb35a
Pranava Adiga <66902872+adigapranava@users.noreply.github.com>
2025-08-18 18:39:39 +0530

    Add solution for the "Fruit Into Baskets" problem using a sliding window approach

---
+++ twoPointers/SLIDING_WINDOW/904_fruit_into_baskets.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<numeric>
+using namespace std;
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int left =0;
+        unordered_map<int, int> basket;
+        int max_fruits= 0;
+        int fruit_type=0;
+
+        for (int right = 0; right < fruits.size(); right++)
+        {
+            basket[fruits[right]]+=1;
+            fruit_type = basket[fruits[right]] == 1 ? fruit_type+1: fruit_type;
+            
+            if (fruit_type > 2)
+            {
+                basket[fruits[left]]-=1;
+                fruit_type = basket[fruits[left]] == 0 ? fruit_type-1: fruit_type;
+                left++;
+            }
+            max_fruits = max(max_fruits, right-left+1);
+        }
+        return max_fruits;
+    }
+};
+
+
+int main() {
+    Solution sol;
+    vector<int> fruits = {1,2,3,2,2}; // Example input
+    int result = sol.totalFruit(fruits);
+    
+    cout << "Maximum number of fruits in two baskets: " << result << endl; // Output the result
+
+    return 0;
+}
